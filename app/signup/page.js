0b052156f8ade9c41048a8c70819eda4a4ee8906
Alireza Malak Mohammadi <alirezamalekmo1@gmail.com>
2025-06-02 12:05:28 +0330

    Edit sign up page title

--- app/signup/page.js
+++ app/signup/page.js
@@ -5,7 +5,7 @@ export default function page() {
         <div className="flex flex-col gap-10 mt-10 items-center">
     
           <form action={createGuest} className="border border-primary-800 px-16 py-12 text-center">
-              <h1 className="font-bold text-2xl ">Login</h1>
+              <h1 className="font-bold text-2xl ">Sign Up</h1>
               {/* {error ? <h2 className="text-xl text-red-700">{errorMessage[error] || errorMessage.default}</h2> : ""} */}
             <div className="mt-8">
               <label className="text-xl" htmlFor="">Email: </label>
@@ -20,7 +20,7 @@ export default function page() {
               <label className="text-xl" htmlFor="">Password: </label>
               <input required name="password" className="bg-primary-950 outline-none border border-primary-800 rounded-md w-52 px-2 py-2 ml-2" type="text" />
             </div>
-            <button type="submit" className="px-10 py-2 text-xl bg-primary-600 rounded-md transition hover:bg-primary-800 mt-8">Login</button>
+            <button type="submit" className="px-10 py-2 text-xl bg-primary-600 rounded-md transition hover:bg-primary-800 mt-8">Sign Up</button>
           </form>
     
         </div>
