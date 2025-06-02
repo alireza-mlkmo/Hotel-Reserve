12f7f2b000538c8c41731e714e8484abfedcf2d6
Alireza Malak Mohammadi <alirezamalekmo1@gmail.com>
2025-06-02 16:47:00 +0330

    Edit env file

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const SUPABASE_URL="https://cjjblomvvijynrktyksu.supabase.co"
-const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqamJsb212dmlqeW5ya3R5a3N1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDkwNzA3MiwiZXhwIjoyMDU2NDgzMDcyfQ.uMwOeRKMqLoPG7ML0Ypl0-xyo2j_VHHnk6GHfL_LjdU"
 
 export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 );
